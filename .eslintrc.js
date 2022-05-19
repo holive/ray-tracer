7f5e40fb6da9ee228fe86c3904f283a5fe3dcd5e
Hugo Oliveira <hbliveira@gmail.com>
2022-05-18 23:13:23 -0300

    chore: add start script and updated deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,32 +2,26 @@
 
 module.exports = {
   root: true,
-
   plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
-
   env: {
     commonjs: true,
     es6: true,
     jest: true,
     node: true,
   },
-
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
   },
-
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier',
   ],
-
   rules: {
     'arrow-body-style': ['error', 'as-needed'],
     'prefer-arrow-callback': ['error'],
