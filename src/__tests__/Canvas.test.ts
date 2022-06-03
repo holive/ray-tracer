119a47aaf4349d9728e52a13948dd046b5e3e08a
Hugo Oliveira <hbliveira@gmail.com>
2022-06-03 09:40:58 -0300

    test: format ppm string

--- src/__tests__/Canvas.test.ts
+++ src/__tests__/Canvas.test.ts
@@ -1,5 +1,7 @@
 import { Canvas } from '../Canvas'
-import { BLACK, Color } from '../tuples'
+import { BLACK, Color, WHITE } from '../tuples'
+
+const COLOR_START_LINE = 3
 
 describe('Canvas', () => {
   it('should create a canvas', () => {
@@ -15,5 +17,42 @@ describe('Canvas', () => {
     const red = new Color(1, 0, 0)
     c.writePixel(2, 3, red)
     expect(c.pixelAt(2, 3)).toEqual(red)
+  })
+
+  it('should construct the PPM header', () => {
+    const ppm = new Canvas(5, 3).toPPM().split('\n')
+    expect(ppm[0]).toBe('P3')
+    expect(ppm[1]).toBe('5 3')
+    expect(ppm[2]).toBe('255')
+  })
+
+  it('should construct the PPM pixel data', () => {
+    const c = new Canvas(5, 3)
+    const c1 = new Color(1.5, 0, 0)
+    const c2 = new Color(0, 0.5, 0)
+    const c3 = new Color(-0.5, 0, 1)
+
+    c.writePixel(0, 0, c1)
+    c.writePixel(2, 1, c2)
+    c.writePixel(4, 2, c3)
+
+    const ppm = c.toPPM()
+    const lines = ppm.split('\n')
+
+    expect(lines[COLOR_START_LINE]).toBe('255 0 0 0 0 0 0 0 0 0 0 0 0 0 0')
+    expect(lines[COLOR_START_LINE + 1]).toBe('0 0 0 0 0 0 0 128 0 0 0 0 0 0 0')
+    expect(lines[COLOR_START_LINE + 2]).toBe('0 0 0 0 0 0 0 0 0 0 0 0 0 0 255')
+    expect(ppm.substring(ppm.length - 1)).toBe('\n') // some image programs require a new line character at the end
+  })
+
+  it('should not write more than 70 characters per line', () => {
+    const width = 100
+    const c = new Canvas(width, 1)
+    for (let i = 0; i < width; i++) {
+      c.writePixel(i, 0, WHITE)
+    }
+
+    const ppm = c.toPPM().split('\n')
+    expect(ppm[COLOR_START_LINE].length < 70).toBeTruthy()
   })
 })
