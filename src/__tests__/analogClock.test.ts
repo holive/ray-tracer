66b14658b2149154b74697abad891496eefafb13
Hugo Oliveira <holive.git@gmail.com>
2022-06-28 09:24:29 -0300

    feat: build a clock with matrix transformations

---
+++ src/__tests__/analogClock.test.ts
@@ -0,0 +1,51 @@
+import { Canvas } from '../Canvas'
+import { ALMOST_WHITE, GRAY, Point, WHITE } from '../tuples'
+import { Matrix } from '../matrices/Matrix'
+import { degreesToRadians } from '../utils'
+import { MatrixTypeFour } from '../matrices/types'
+
+describe('build a analog clock to put together matrices operations', () => {
+  it('write the clock in the ppm file', () => {
+    const width = 1000
+    const height = 1000
+    const c = new Canvas(width, height)
+    const initialPosition = new Point(0, width * 0.1 * 4, 0)
+
+    for (let pointer = 0; pointer <= 360; pointer += 30) {
+      const transformMatrix = new Matrix(
+        transformationMatrix(width * 0.5, height * 0.5, -pointer)
+      )
+      const transformed = transformMatrix.multiplyByTuple(initialPosition)
+
+      writePixel(c, transformed.x, transformed.y)
+    }
+
+    c.toPPM()
+  })
+})
+
+function transformationMatrix(
+  toX: number,
+  toY: number,
+  rotation: number
+): MatrixTypeFour {
+  const r = Matrix.rotationZ(degreesToRadians(rotation))
+  const s = Matrix.scaling(1, 1, 1)
+  const t = Matrix.translation(toX, toY, 0)
+
+  return new Matrix(new Matrix(t).multiply(s)).multiply(r)
+}
+
+function writePixel(c: Canvas, x: number, y: number) {
+  c.writePixel(x, y, WHITE)
+
+  c.writePixel(x - 1, y, ALMOST_WHITE)
+  c.writePixel(x + 1, y, ALMOST_WHITE)
+  c.writePixel(x, y - 1, ALMOST_WHITE)
+  c.writePixel(x, y + 1, ALMOST_WHITE)
+
+  c.writePixel(x + 1, y + 1, GRAY)
+  c.writePixel(x + 1, y - 1, GRAY)
+  c.writePixel(x - 1, y + 1, GRAY)
+  c.writePixel(x - 1, y - 1, GRAY)
+}
--- src/tuples/constants.ts
+++ src/tuples/constants.ts
@@ -2,3 +2,5 @@ import { Color } from './Color'
 
 export const BLACK = new Color(0, 0, 0)
 export const WHITE = new Color(255, 255, 255)
+export const ALMOST_WHITE = new Color(0.9, 0.9, 0.9)
+export const GRAY = new Color(32 * 0.01, 32 * 0.01, 32 * 0.01)
