3d86009a5c047536aec0be809525298060bfb6e2
Hugo Oliveira <holive.git@gmail.com>
2022-07-07 22:30:18 -0300

    fix: intersect method

--- src/__tests__/spheres.test.ts
+++ src/__tests__/spheres.test.ts
@@ -6,8 +6,8 @@ import { IDENTITY_MATRIX, Matrix } from '../matrices'
 describe('Spheres', () => {
   it('A ray intersects a sphere at two points', () => {
     const ray = new Ray(new Point(0, 0, -5), new Vector(0, 0, 1))
-    const sphere = new Sphere('2j3b4')
-    const intersections = sphere.intersect(ray)
+    const sphere = new Sphere()
+    const intersections = ray.intersect(sphere)
     expect(intersections.length).toBe(2)
     expect(intersections[0].t).toBe(4)
     expect(intersections[1].t).toBe(6)
@@ -15,8 +15,8 @@ describe('Spheres', () => {
 
   it('should check if a ray intersects a sphere at a tangent', () => {
     const ray = new Ray(new Point(0, 1, -5), new Vector(0, 0, 1))
-    const sphere = new Sphere('323fsd')
-    const intersections = sphere.intersect(ray)
+    const sphere = new Sphere()
+    const intersections = ray.intersect(sphere)
     expect(intersections.length).toBe(2)
     expect(intersections[0].t).toBe(5)
     expect(intersections[1].t).toBe(5)
@@ -24,15 +24,15 @@ describe('Spheres', () => {
 
   it('should check if a ray misses a sphere', () => {
     const ray = new Ray(new Point(0, 2, -5), new Vector(0, 0, 1))
-    const sphere = new Sphere('asdf3')
-    const intersections = sphere.intersect(ray)
+    const sphere = new Sphere()
+    const intersections = ray.intersect(sphere)
     expect(intersections.length).toBe(0)
   })
 
   it('should check if a ray originates inside a sphere', () => {
     const ray = new Ray(new Point(0, 0, 0), new Vector(0, 0, 1))
-    const sphere = new Sphere('1234ad')
-    const intersections = sphere.intersect(ray)
+    const sphere = new Sphere()
+    const intersections = ray.intersect(sphere)
     expect(intersections.length).toBe(2)
     expect(intersections[0].t).toBe(-1)
     expect(intersections[1].t).toBe(1)
@@ -40,8 +40,8 @@ describe('Spheres', () => {
 
   it('should check if a sphere is behind a ray', () => {
     const ray = new Ray(new Point(0, 0, 5), new Vector(0, 0, 1))
-    const sphere = new Sphere('32ewds')
-    const intersections = sphere.intersect(ray)
+    const sphere = new Sphere()
+    const intersections = ray.intersect(sphere)
     expect(intersections.length).toBe(2)
     expect(intersections[0].t).toBe(-6)
     expect(intersections[1].t).toBe(-4)
@@ -49,27 +49,27 @@ describe('Spheres', () => {
 
   it('should check if Intersect sets the object on the intersection', () => {
     const ray = new Ray(new Point(0, 0, -5), new Vector(0, 0, 1))
-    const sphere = new Sphere('23wd')
-    const intersections = sphere.intersect(ray)
+    const sphere = new Sphere()
+    const intersections = ray.intersect(sphere)
     expect(intersections.length).toBe(2)
     expect(intersections[0].object).toEqual(sphere)
     expect(intersections[1].object).toEqual(sphere)
   })
 
   it('should return the default sphere transformation', () => {
-    const s = new Sphere('a3d')
+    const s = new Sphere()
     expect(s.getTransform()).toEqual(IDENTITY_MATRIX)
   })
 
   it('should replace the default sphere transformation', () => {
-    const s = new Sphere('a3d')
+    const s = new Sphere()
     const matrix = Matrix.translation(2, 3, 4)
     s.setTransform(matrix)
     expect(s.getTransform()).toEqual(matrix)
   })
 
   it('should replace the default sphere transformation', () => {
-    const s = new Sphere('a3d')
+    const s = new Sphere()
     const matrix = Matrix.translation(2, 3, 4)
     s.setTransform(matrix)
     expect(s.getTransform()).toEqual(matrix)
@@ -77,9 +77,9 @@ describe('Spheres', () => {
 
   it('should intersect a scaled sphere with a ray', () => {
     const r = new Ray(new Point(0, 0, -5), new Vector(0, 0, 1))
-    const s = new Sphere('a3d')
+    const s = new Sphere()
     s.setTransform(Matrix.scaling(2, 2, 2))
-    const xs = s.intersect(r)
+    const xs = r.intersect(s)
     expect(xs.length).toBe(2)
     expect(xs[0].t).toBe(3)
     expect(xs[1].t).toBe(7)
@@ -87,9 +87,9 @@ describe('Spheres', () => {
 
   it('should intersect a translated sphere with a ray', () => {
     const r = new Ray(new Point(0, 0, -5), new Vector(0, 0, 1))
-    const s = new Sphere('a3d')
+    const s = new Sphere()
     s.setTransform(Matrix.translation(5, 0, 0))
-    const xs = s.intersect(r)
+    const xs = r.intersect(s)
     expect(xs.length).toBe(0)
   })
 })
--- src/spheres/Sphere.ts
+++ src/spheres/Sphere.ts
@@ -1,39 +1,10 @@
-import { Point, Vector } from '../tuples'
-import { Ray } from '../rays'
-import { sq } from '../utils'
-import { Intersection } from '../intersections'
-import { IDENTITY_MATRIX, Matrix, MatrixTypeFour } from '../matrices'
+import { Point } from '../tuples'
+
+import { IDENTITY_MATRIX, MatrixTypeFour } from '../matrices'
 
 export class Sphere {
-  id: string
   position = new Point(0, 0, 0)
   private transform: MatrixTypeFour = IDENTITY_MATRIX
-
-  constructor(id: string) {
-    this.id = id
-  }
-
-  intersect(ray: Ray): Intersection[] {
-    const newRay = this.transformRay(ray)
-
-    // the vector from the sphere's center, to the ray origin
-    // remember: the sphere is centered at the world origin
-    const { x, y, z } = newRay.origin.subtract(this.position)
-    const sphereToRay = new Vector(x, y, z)
-
-    const a = newRay.direction.dot(newRay.direction)
-    const b = 2 * newRay.direction.dot(sphereToRay)
-    const c = sphereToRay.dot(sphereToRay) - 1
-    const discriminant = sq(b) - 4 * a * c
-
-    if (discriminant < 0) {
-      return []
-    }
-
-    const t1 = (-b - Math.sqrt(discriminant)) / (2 * a)
-    const t2 = (-b + Math.sqrt(discriminant)) / (2 * a)
-    return [new Intersection(t1, this), new Intersection(t2, this)]
-  }
 
   getTransform(): MatrixTypeFour {
     return this.transform
@@ -41,14 +12,5 @@ export class Sphere {
 
   setTransform(matrix: MatrixTypeFour): void {
     this.transform = matrix
-  }
-
-  private transformRay(ray: Ray): Ray {
-    const matrix = Matrix.inverse(this.transform) as MatrixTypeFour
-    if (!matrix || matrix.length != 4) {
-      throw new Error('Cannot transform this ray.')
-    }
-
-    return ray.transform(new Matrix(matrix))
   }
 }
