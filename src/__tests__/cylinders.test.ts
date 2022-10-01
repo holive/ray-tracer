68a6255fee32c5c6a004d300e1809c54e9853094
Hugo Oliveira <holive.git@gmail.com>
2022-10-01 11:47:17 -0300

    feat: a ray strikes a cyllinder

--- src/__tests__/cylinders.test.ts
+++ src/__tests__/cylinders.test.ts
@@ -27,4 +27,37 @@ describe('Cylinders', () => {
       expect(xs.length).toBe(0)
     })
   })
+
+  it('checks if a ray strikes a cylinder', () => {
+    const cases = [
+      {
+        origin: new Point(1, 0, -5),
+        direction: new Vector(0, 0, 1),
+        t0: 5,
+        t1: 5
+      },
+      {
+        origin: new Point(0, 0, -5),
+        direction: new Vector(0, 0, 1),
+        t0: 4,
+        t1: 6
+      },
+      {
+        origin: new Point(0.5, 0, -5),
+        direction: new Vector(0.1, 1, 1),
+        t0: 6.80798,
+        t1: 7.08872
+      }
+    ]
+
+    cases.forEach((cs) => {
+      const cyl = new Cylinder()
+      const direction = cs.direction.normalize()
+      const r = new Ray(cs.origin, direction)
+      const xs = cyl.localIntersect(r)
+      expect(xs.length).toBe(2)
+      expect(+xs[0].t.toFixed(5)).toBe(cs.t0)
+      expect(+xs[1].t.toFixed(5)).toBe(cs.t1)
+    })
+  })
 })
--- src/cylinders/cylinder.ts
+++ src/cylinders/cylinder.ts
@@ -1,12 +1,13 @@
 import { BaseShape } from '../shapes'
 import { Ray } from '../rays'
 import { Intersection } from '../intersections'
+import { EPSILON } from '../utils'
 
 export class Cylinder extends BaseShape {
   localIntersect(r: Ray): Intersection[] {
     const a = r.direction.x ** 2 + r.direction.z ** 2
 
-    if (Math.round(a / 1000) * 1000 == 0) return []
+    if (Math.abs(a) < EPSILON) return []
 
     const b = 2 * r.origin.x * r.direction.x + 2 * r.origin.z * r.direction.z
     const c = r.origin.x ** 2 + r.origin.z ** 2 - 1
@@ -14,6 +15,9 @@ export class Cylinder extends BaseShape {
 
     if (disc < 0) return []
 
-    return [new Intersection(1, this)]
+    const t0 = (-b - Math.sqrt(disc)) / (2 * a)
+    const t1 = (-b + Math.sqrt(disc)) / (2 * a)
+
+    return [new Intersection(t0, this), new Intersection(t1, this)]
   }
 }
